c521382bf55756648f69392199c606be66d762f0
mar1n <30738216+mar1n@users.noreply.github.com>
2020-07-02 16:46:43 +0100

    pass prop up to parent

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,13 +5,30 @@ const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="Menu">
-      <button className="Menu-button" onClick={() => setIsOpen(!isOpen)}>Menu</button>
-        <div className={`Menu-box ${isOpen ? 'is-open' : ''}`}>
-          <div className="Menu-box-link">Home</div>
-          <div className="Menu-box-link">Projects</div>
-          <div className="Menu-box-link">About</div>
-          <div className="Menu-box-link">Contact</div>
-        </div>
+      <MenuButton 
+        isOpem={isOpen}
+        onMenu={() => setIsOpen(!isOpen)}
+        />
+      <MenuBox  isOpen={isOpen}/>
+    </div>
+  );
+};
+
+const MenuButton = ({ onMenu }) => {
+  return (
+    <button className="Menu-button" onClick={() => onMenu()}>
+      Menu
+    </button>
+  );
+};
+
+const MenuBox = ({ isOpen }) => {
+  return (
+    <div className={`Menu-box ${isOpen ? "is-open" : ""}`}>
+      <div className="Menu-box-link">Home</div>
+      <div className="Menu-box-link">Projects</div>
+      <div className="Menu-box-link">About</div>
+      <div className="Menu-box-link">Contact</div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Specificity from './components/Specificity/Specificity'
 import Menu from './components/Menu/Menu';
 import * as serviceWorker from './serviceWorker';
 
